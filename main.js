5f3c3bca93ac4a6652791d4d703089b4c08548a7
Ivan Dario Niello <ivaniello@hotmail.com>
2022-04-02 15:11:49 -0300

    desafio

---
+++ main.js
@@ -0,0 +1,28 @@
+// carrito de compras de un negocio
+class logIn {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+//const xxElwachin_maspiolaxx = NewLogIn (xxElwachin_maspiolaxx, elwachin@gmail.com, pass1234);
+
+const precioTotal = (a, b) => a+b
+let remera = 1700;
+let gorra = 600;
+let mousepad = 2100;
+
+console.log(precioTotal(1500,1600))
+
+precioTotal (remera, gorra);
+console.log (precioTotal(remera, gorra))
+stock = 2
+while (stock > 0) {
+    alert("En Stock"); 
+    break;
+} if (stock==0) {
+    alert ("No hay Stock en este momento. Vuelva más tarde o contactese con nosotros")
+}
+
